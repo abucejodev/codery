7f996ad7c075d0b6092f7ef98ce099675a3f85f3
nikkoabucejo <me@nikkoabucejo.com>
2023-02-14 16:05:53 +0800

    chore: update fetcher to use axios

--- core/providers/query.tsx
+++ core/providers/query.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import { SWRConfig, type SWRConfiguration } from "swr";
+import axios from "axios";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Query = ({ children }: Props) => {
-  const fetcher = async (key: string) => {
-    const response = await fetch(key);
-    const data = await response.json();
+  const fetcher = (key: string) => {
+    const data = axios.get(key).then((response) => response.data);
     return data;
   };
 
