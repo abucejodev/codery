525a6e624406b3a8a7a0049ea5b98232de173cd9
nikkoabucejo <me@nikkoabucejo.com>
2023-02-15 17:51:25 +0800

    chore(theme): change default breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   darkMode: "class",
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./core/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      landscape: "480px",
+      tablet: "640px",
+      laptop: "1024px",
+      desktop: "1280px",
+    },
     extend: {},
   },
   plugins: [],
