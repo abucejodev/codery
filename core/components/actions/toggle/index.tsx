a8ed8c8d7eacf3095f0fb1c347587d663657601a
nikkoabucejo <me@nikkoabucejo.com>
2023-02-17 01:17:50 +0800

    chore: add dark mode support

--- core/components/actions/toggle/index.tsx
+++ core/components/actions/toggle/index.tsx
@@ -16,13 +16,14 @@ const Toggle = ({ isEnabled = false, onChange = () => {} }: Props) => {
       className={cx([
         "h-6 w-12 cursor-pointer rounded-full border-2 border-transparent focus:outline-none",
         "flex-none transition-colors duration-200 ease-in-out",
-        `${isEnabled ? "bg-dark" : "bg-dark/20"}
+        `${isEnabled ? "bg-night dark:bg-day" : "bg-night/20 dark:bg-day/20"}
       `,
       ])}>
       <span
         aria-hidden="true"
         className={cx([
-          "pointer-events-none inline-block h-5 w-5 rounded-full bg-light ring-0",
+          "pointer-events-none inline-block h-5 w-5 rounded-full bg-day ring-0",
+          "dark:bg-night",
           "transition duration-200 ease-in-out",
           `${isEnabled ? "translate-x-3" : "-translate-x-3"}
         `,
