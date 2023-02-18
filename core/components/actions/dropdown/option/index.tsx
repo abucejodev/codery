e7640d6ebc553951549824ed6e00460a276226ce
nikkoabucejo <me@nikkoabucejo.com>
2023-02-18 13:35:21 +0800

    chore: add on click active effect

--- core/components/actions/dropdown/option/index.tsx
+++ core/components/actions/dropdown/option/index.tsx
@@ -25,6 +25,8 @@ const Option = ({
           className={cx([
             "flex w-full items-center gap-2 rounded p-2 text-sm text-obsidian",
             "dark:text-quartz",
+            "active:scale-95",
+            "transition-all duration-100 ease-in",
             // prettier-ignore
             `${active ? "bg-obsidian/10 dark:bg-quartz/10 dark:text-quartz" : ""}
              ${disabled ? "text-obsidian/50 dark:text-quartz/50" : ""}`,
