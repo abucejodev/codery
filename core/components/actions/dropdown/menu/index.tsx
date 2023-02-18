dc31d1b014b5d50f98fad247c8e9e8c4800d1a24
nikkoabucejo <me@nikkoabucejo.com>
2023-02-18 15:18:49 +0800

    chore: add character component support

--- core/components/actions/dropdown/menu/index.tsx
+++ core/components/actions/dropdown/menu/index.tsx
@@ -5,7 +5,7 @@ import { Fragment } from "react";
 import purge from "@/core/libraries/purge";
 import variants, { type Variants } from "../../variants";
 import { cx } from "class-variance-authority";
-import Avatar from "@/core/components/figures/avatar";
+import Character from "@/core/components/figures/character";
 
 type Props = Pick<Variants, "intent" | "volume" | "theme"> & {
   theme?: Extract<
@@ -31,44 +31,36 @@ const Menu = ({
 }: Props) => {
   return (
     <HeadlessMenu as="div" className="relative inline-block text-left">
-      {({ open }) => (
-        <>
-          <HeadlessMenu.Button className={purge(variants(props))}>
-            {avatar ? <Avatar alt="Me" src={avatar} /> : <></>}
-            {Icon ? <Icon className="h-4 w-4 stroke-2" /> : <></>}
-            {name && !about ? <span>{name}</span> : <></>}
-            {name && about ? (
-              <div className="flex flex-col">
-                <span>{name}</span>
-                <span className="text-xs font-normal opacity-80">{about}</span>
-              </div>
-            ) : (
-              <></>
-            )}
-          </HeadlessMenu.Button>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95">
-            <HeadlessMenu.Items
-              className={cx([
-                "absolute z-10 w-64 divide-y divide-obsidian/10 rounded bg-quartz shadow-lg",
-                "dark:divide-quartz/10 dark:bg-obsidian dark:text-quartz",
-                "focus:outline-none",
-                `${position === "top/left" ? "bottom-full left-0 mb-2" : ""}`,
-                `${position === "top/right" ? "bottom-full right-0 mb-2" : ""}`,
-                `${position === "bottom/left" ? "top-full left-0 mt-2" : ""}`,
-                `${position === "bottom/right" ? "top-full right-0 mt-2" : ""}`,
-              ])}>
-              {children}
-            </HeadlessMenu.Items>
-          </Transition>
-        </>
-      )}
+      <HeadlessMenu.Button className={purge(variants(props))}>
+        {Icon ? <Icon className="h-4 w-4 stroke-2" /> : <></>}
+        {name && !about ? <span>{name}</span> : <></>}
+        {name && about ? (
+          <Character name={name} about={about} avatar={avatar} />
+        ) : (
+          <></>
+        )}
+      </HeadlessMenu.Button>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95">
+        <HeadlessMenu.Items
+          className={cx([
+            "absolute z-10 w-64 divide-y divide-obsidian/10 rounded bg-quartz shadow-lg",
+            "dark:divide-quartz/10 dark:bg-obsidian dark:text-quartz",
+            "focus:outline-none",
+            `${position === "top/left" ? "bottom-full left-0 mb-2" : ""}`,
+            `${position === "top/right" ? "bottom-full right-0 mb-2" : ""}`,
+            `${position === "bottom/left" ? "top-full left-0 mt-2" : ""}`,
+            `${position === "bottom/right" ? "top-full right-0 mt-2" : ""}`,
+          ])}>
+          {children}
+        </HeadlessMenu.Items>
+      </Transition>
     </HeadlessMenu>
   );
 };
