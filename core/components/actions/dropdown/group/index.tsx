1af4c43f9ccddbd32c4e74043619f5562b08c28f
nikkoabucejo <me@nikkoabucejo.com>
2023-02-18 10:43:12 +0800

    chore(dropdown): add name prop

--- core/components/actions/dropdown/group/index.tsx
+++ core/components/actions/dropdown/group/index.tsx
@@ -1,10 +1,20 @@
 type Props = {
   children: React.ReactNode;
+  name?: string;
   className?: string;
 };
 
-const Group = ({ children, className }: Props) => {
-  return <div className={`${className ? className : ""} p-1`}> {children}</div>;
+const Group = ({ children, name, className }: Props) => {
+  return (
+    <div className={`${className ? className : ""} p-1`}>
+      {name ? (
+        <span className="block p-2 text-sm font-medium">{name}</span>
+      ) : (
+        <></>
+      )}
+      {children}
+    </div>
+  );
 };
 
 export default Group;
