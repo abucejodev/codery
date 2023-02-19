97f8bec9278b2b8ec8567d863dbf05fc3c6242f1
jazztine <jzztn.crz@gmail.com>
2023-02-19 21:05:49 +0800

    chore(fields): create radio

---
+++ core/components/fields/radio/index.tsx
@@ -0,0 +1,26 @@
+import TFieldProps from "@/core/types/field";
+
+const Radio =({ name, ...props }: TFieldProps) => {
+  return (
+    <div className="flex items-center">
+      <input
+        type="radio"
+        id={props.id}
+        name={name}
+        value={props.value}
+        defaultValue={props.defaultValue}
+        defaultChecked={props.defaultChecked}
+        placeholder={props.placeholder}
+        required={props.isRequired}
+        disabled={props.isDisabled}
+        readOnly={props.readOnly}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+        className={`${props.className ? props.className : ""}`}
+      />
+      {props.children}
+    </div>
+  );
+};
+
+export default Radio;
