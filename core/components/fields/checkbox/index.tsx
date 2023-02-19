ac3f0f74fd5b07a87ed09707d95eaa9fb22c001d
jazztine <jzztn.crz@gmail.com>
2023-02-19 21:06:09 +0800

    chore(fields): create checkbox

---
+++ core/components/fields/checkbox/index.tsx
@@ -0,0 +1,26 @@
+import TFieldProps from "@/core/types/field";
+
+const CheckBox = ({ name, ...props }: TFieldProps) => {
+  return (
+    <div className="flex items-center">
+      <input
+        type="checkbox"
+        id={props.id}
+        name={name}
+        value={props.value}
+        defaultValue={props.defaultValue}
+        defaultChecked={props.defaultChecked}
+        placeholder={props.placeholder}
+        required={props.isRequired}
+        disabled={props.isDisabled}
+        readOnly={props.readOnly}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+        className={`${props.className ? props.className : ""}`}
+      />
+      {props.children}
+    </div>
+  );
+};
+
+export default CheckBox;
