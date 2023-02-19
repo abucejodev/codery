e9b38665f039d2a2095ec63f7ea4a998f708eca9
jazztine <jzztn.crz@gmail.com>
2023-02-19 23:22:47 +0800

    chore(fields): create body

---
+++ core/components/fields/body/index.tsx
@@ -0,0 +1,28 @@
+import TField from "@/core/types/field";
+import Description from "../description";
+import ErrorMessage from "../error-message";
+import Label from "../label";
+
+const Body = ({
+  id,
+  children,
+  label,
+  errors,
+  description,
+  tooltip,
+  Icon,
+}: TField) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center gap-3">
+        {label ? <Label name={label} id={id} /> : null}
+        {tooltip ? <div>{tooltip}</div> : null}
+      </div>
+      {description ? <Description paragraph={description} /> : null}
+      {children}
+      <ErrorMessage Icon={Icon} message={errors?.message} />
+    </div>
+  );
+};
+
+export default Body;
