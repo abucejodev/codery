5ee727d1214f1d2a92fccc22566e02cd609f082d
jazztine <jzztn.crz@gmail.com>
2023-02-19 23:21:50 +0800

    chore(fields): create email

---
+++ core/components/fields/email/index.tsx
@@ -0,0 +1,28 @@
+import TField from "@/core/types/field";
+
+const Email = ({ name, ...props }: TField) => {
+  return (
+    <div className="flex items-center">
+      {props.children}
+
+      <input
+        type="email"
+        id={props.id}
+        name={name}
+        value={props.value}
+        defaultValue={props.defaultValue}
+        placeholder={props.placeholder}
+        required={props.isRequired}
+        disabled={props.isDisabled}
+        readOnly={props.readOnly}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+        className={`${
+          props.className ? props.className : ""
+        } placeholder:text-xs placeholder:font-semibold placeholder:text-obsidian/50`}
+      />
+    </div>
+  );
+};
+
+export default Email;
