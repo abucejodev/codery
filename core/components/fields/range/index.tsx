53c4e123888ae823745218cc6d8e57b1db953523
jazztine <jzztn.crz@gmail.com>
2023-02-21 19:14:52 +0800

    chore(fields): add temporary design for range

--- core/components/fields/range/index.tsx
+++ core/components/fields/range/index.tsx
@@ -12,24 +12,30 @@ const Range = ({ name, ...props }: TField) => {
       description={props.description}
       errors={props.errors}
       tooltip={props.tooltip}>
-      <input
-        type="range"
-        id={props.id}
-        name={name}
-        value={props.value}
-        defaultValue={props.defaultValue}
-        defaultChecked={props.defaultChecked}
-        min={props.min}
-        max={props.max}
-        step={props.step}
-        placeholder={props.placeholder}
-        required={props.isRequired}
-        disabled={props.isDisabled}
-        readOnly={props.readOnly}
-        onChange={props.onChange}
-        onBlur={props.onBlur}
-        className={`${props.className ? props.className : ""}`}
-      />
+      <div
+        className="group grid w-fit cursor-pointer place-items-center rounded border border-transparent transition-all duration-300 hover:border-obsidian/20 hover:bg-obsidian/20 active:border-obsidian/20 active:bg-obsidian/20
+  ">
+        <input
+          type="range"
+          id={props.id}
+          name={name}
+          value={props.value}
+          defaultValue={props.defaultValue}
+          defaultChecked={props.defaultChecked}
+          min={props.min}
+          max={props.max}
+          step={props.step}
+          placeholder={props.placeholder}
+          required={props.isRequired}
+          disabled={props.isDisabled}
+          readOnly={props.readOnly}
+          onChange={props.onChange}
+          onBlur={props.onBlur}
+          className={`${
+            props.className ? props.className : ""
+          } cursor-pointer rounded border border-transparent bg-obsidian/5 p-2 text-sm  font-semibold text-obsidian/60 outline-none transition-all duration-300 focus:border-obsidian/20 focus:bg-white group-hover:border-obsidian/20 group-hover:bg-white`}
+        />
+      </div>
     </Fields.Body>
   );
 };
