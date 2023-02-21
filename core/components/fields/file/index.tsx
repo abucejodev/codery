df9af20d662ce1b10fde19452c7396a0d87374d9
nikkoabucejo <me@nikkoabucejo.com>
2023-02-21 21:24:17 +0800

    chore: update file field

--- core/components/fields/file/index.tsx
+++ core/components/fields/file/index.tsx
@@ -1,6 +1,7 @@
 import type TField from "@/core/types/fields";
 import { useId } from "react";
 import Fields from "..";
+import Shell from "../_shell";
 
 type Props = TField<{
   isMultiple?: boolean;
@@ -14,36 +15,23 @@ const File = ({ name, ...props }: Props) => {
   const id = useId();
 
   return (
-    <Fields.Body
+    <Shell
       id={id}
       label={props.label}
       description={props.description}
       errors={props.errors}
       tooltip={props.tooltip}>
-      <div
-        className="group grid w-fit cursor-pointer place-items-center rounded border border-transparent transition-all duration-300 hover:border-obsidian/20 hover:bg-obsidian/20 active:border-obsidian/20 active:bg-obsidian/20
-  ">
+      <div className="group grid w-fit cursor-pointer place-items-center rounded border border-transparent transition-all duration-300 hover:border-obsidian/20 hover:bg-obsidian/20 active:border-obsidian/20 active:bg-obsidian/20">
         <input
+          {...props}
           type="file"
-          id={props.id}
-          name={name}
-          value={props.value}
-          defaultValue={props.defaultValue}
-          placeholder={props.placeholder}
-          required={props.isRequired}
-          disabled={props.isDisabled}
-          readOnly={props.readOnly}
-          accept={props.accept}
-          capture={props.capture}
-          multiple={props.isMultiple}
-          onChange={props.onChange}
-          onBlur={props.onBlur}
+          id={id}
           className={`${
             props.className ? props.className : ""
           } cursor-pointer rounded border border-transparent bg-obsidian/5 p-2 text-sm  font-semibold text-obsidian/60 outline-none transition-all duration-300 focus:border-obsidian/20 focus:bg-white group-hover:border-obsidian/20 group-hover:bg-white`}
         />
       </div>
-    </Fields.Body>
+    </Shell>
   );
 };
 
