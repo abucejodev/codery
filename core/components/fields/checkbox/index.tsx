04bd618d8e8a6235801940382f235e9843cdee82
nikkoabucejo <me@nikkoabucejo.com>
2023-02-21 21:34:22 +0800

    chore: update checkbox field

--- core/components/fields/checkbox/index.tsx
+++ core/components/fields/checkbox/index.tsx
@@ -1,45 +1,35 @@
-import type TField from "@/core/types/fields";
+import type Fields from "@/core/types/fields";
 import { useId } from "react";
-import Fields from "..";
+import Shell from "../_shell";
 
-type Props = TField<{
+type Props = Fields<{
   defaultChecked?: boolean;
   value?: string;
   placeholder?: string;
 }>;
 
-const CheckBox = ({ name, ...props }: Props) => {
+const Checbox = ({ label, description, errors, tooltip, ...props }: Props) => {
   const id = useId();
 
   return (
-    <Fields.Body
+    <Shell
       id={id}
-      label={props.label}
-      description={props.description}
-      errors={props.errors}
-      tooltip={props.tooltip}>
-      <div
-        className="group grid w-fit cursor-pointer place-items-center rounded border border-transparent transition-all duration-300 hover:border-obsidian/20 hover:bg-obsidian/20 active:border-obsidian/20 active:bg-obsidian/20
-  ">
+      label={label}
+      description={description}
+      errors={errors}
+      tooltip={tooltip}>
+      <div className="group grid w-fit cursor-pointer place-items-center rounded border border-transparent transition-all duration-300 hover:border-obsidian/20 hover:bg-obsidian/20 active:border-obsidian/20 active:bg-obsidian/20">
         <input
           type="checkbox"
           id={id}
-          name={name}
-          value={props.value}
-          defaultChecked={props.defaultChecked}
-          placeholder={props.placeholder}
-          required={props.isRequired}
-          disabled={props.isDisabled}
-          readOnly={props.readOnly}
-          onChange={props.onChange}
-          onBlur={props.onBlur}
+          {...props}
           className={`${
             props.className ? props.className : ""
           } cursor-pointer rounded border border-transparent bg-obsidian/5 p-2 text-sm  font-semibold text-obsidian/60 outline-none transition-all duration-300 focus:border-obsidian/20 focus:bg-white group-hover:border-obsidian/20 group-hover:bg-white`}
         />
       </div>
-    </Fields.Body>
+    </Shell>
   );
 };
 
-export default CheckBox;
+export default Checbox;
