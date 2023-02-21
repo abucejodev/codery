5287ad141f20225ede7f0f8f6b7ede71fef0e057
nikkoabucejo <me@nikkoabucejo.com>
2023-02-21 20:26:53 +0800

    chore: change errors type to string

--- core/types/fields.d.ts
+++ core/types/fields.d.ts
@@ -19,7 +19,7 @@ type TField<Extension> = Extension & {
   readOnly?: boolean;
   onChange?: OnChange;
   onBlur?: OnBlur;
-  errors?: ZodFormattedError;
+  errors?: string;
   className?: string;
 };
 
