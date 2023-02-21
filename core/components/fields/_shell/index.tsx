97b03bcf9689a12461546d1b09715b0219bf9893
nikkoabucejo <me@nikkoabucejo.com>
2023-02-21 20:25:44 +0800

    chore: create shell for fields

---
+++ core/components/fields/_shell/index.tsx
@@ -0,0 +1,42 @@
+import Description from "../description";
+import ErrorMessage from "../error-message";
+
+type Props = {
+  children: React.ReactNode;
+  id?: string;
+  label?: string;
+  description?: string;
+  errors?: string;
+  tooltip?: string;
+};
+
+const Shell = ({
+  id,
+  children,
+  label,
+  errors,
+  description,
+  tooltip,
+}: Props) => {
+  return (
+    <div className="flex max-w-lg flex-col gap-1">
+      <div className="flexflex-col gap-1">
+        <div className="grid grid-cols-[auto,1fr] gap-2">
+          {label ? (
+            <label id={id} className="text-sm font-semibold text-obsidian">
+              {label}
+            </label>
+          ) : (
+            <></>
+          )}
+          {tooltip ? <div>{tooltip}</div> : <></>}
+        </div>
+        {description ? <Description paragraph={description} /> : <></>}
+      </div>
+      {children}
+      {errors ? <ErrorMessage message={errors} /> : <></>}
+    </div>
+  );
+};
+
+export default Shell;
