5d664633135f3f8f82afc2787440169a50a63d3c
jazztine <jzztn.crz@gmail.com>
2023-02-21 19:13:51 +0800

    chore(fields): add temporary design for radio

--- core/components/fields/radio/index.tsx
+++ core/components/fields/radio/index.tsx
@@ -12,21 +12,26 @@ const Radio = ({ name, ...props }: TField) => {
       description={props.description}
       errors={props.errors}
       tooltip={props.tooltip}>
-      <input
-        type="radio"
-        id={props.id}
-        name={name}
-        value={props.value}
-        defaultValue={props.defaultValue}
-        defaultChecked={props.defaultChecked}
-        placeholder={props.placeholder}
-        required={props.isRequired}
-        disabled={props.isDisabled}
-        readOnly={props.readOnly}
-        onChange={props.onChange}
-        onBlur={props.onBlur}
-        className={`${props.className ? props.className : ""}`}
-      />
+      <div
+        className="group grid w-fit cursor-pointer place-items-center rounded border border-transparent transition-all duration-300 hover:border-obsidian/20 hover:bg-obsidian/20 active:border-obsidian/20 active:bg-obsidian/20
+    ">
+        <input
+          type="radio"
+          id={props.id}
+          name={name}
+          value={props.value}
+          defaultValue={props.defaultValue}
+          placeholder={props.placeholder}
+          required={props.isRequired}
+          disabled={props.isDisabled}
+          readOnly={props.readOnly}
+          onChange={props.onChange}
+          onBlur={props.onBlur}
+          className={`${
+            props.className ? props.className : ""
+          } cursor-pointer rounded border border-transparent bg-obsidian/5 p-2 text-sm  font-semibold text-obsidian/60 outline-none transition-all duration-300 focus:border-obsidian/20 focus:bg-white group-hover:border-obsidian/20 group-hover:bg-white`}
+        />
+      </div>
     </Fields.Body>
   );
 };
