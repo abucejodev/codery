7179af59252db05d07666ea56ea5a274bc2fc261
nikkoabucejo <me@nikkoabucejo.com>
2023-02-21 21:32:23 +0800

    chore: update file field

--- core/components/fields/file/index.tsx
+++ core/components/fields/file/index.tsx
@@ -1,31 +1,30 @@
-import type TField from "@/core/types/fields";
+import type Fields from "@/core/types/fields";
 import { useId } from "react";
-import Fields from "..";
 import Shell from "../_shell";
 
-type Props = TField<{
-  isMultiple?: boolean;
+type Props = Fields<{
+  multiple?: boolean;
   accept?: string;
   capture?: boolean | "user" | "environment";
   value?: string;
   placeholder?: string;
 }>;
 
-const File = ({ name, ...props }: Props) => {
+const File = ({ label, description, errors, tooltip, ...props }: Props) => {
   const id = useId();
 
   return (
     <Shell
       id={id}
-      label={props.label}
-      description={props.description}
-      errors={props.errors}
-      tooltip={props.tooltip}>
+      label={label}
+      description={description}
+      errors={errors}
+      tooltip={tooltip}>
       <div className="group grid w-fit cursor-pointer place-items-center rounded border border-transparent transition-all duration-300 hover:border-obsidian/20 hover:bg-obsidian/20 active:border-obsidian/20 active:bg-obsidian/20">
         <input
-          {...props}
           type="file"
           id={id}
+          {...props}
           className={`${
             props.className ? props.className : ""
           } cursor-pointer rounded border border-transparent bg-obsidian/5 p-2 text-sm  font-semibold text-obsidian/60 outline-none transition-all duration-300 focus:border-obsidian/20 focus:bg-white group-hover:border-obsidian/20 group-hover:bg-white`}
